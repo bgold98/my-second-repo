32d64483109e068c22a1170bba2653b5624352d5
bgold98 <golding@umich.edu>
2018-01-30 09:57:32 -0500

    fav food

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print('hello, my name is Ben')
 print("I was born in August")
-print("About me:")
+print("Fav food is steak")
 print('Hello my name is Kate')
 print('I was born in September')
 print("my fav food is sushi")
